18be48759d8da5d470241b31ae91d849d0f96c96
Keeley <keeleymhammond@gmail.com>
2017-12-02 13:40:27 -0800

    Add family data

--- src/data/data.js
+++ src/data/data.js
@@ -13,141 +13,61 @@ const data = [
     category: 'Resource'
   },
   {
-    name: 'Jim and Jane Bananas',
-    shopUrl: 'http://www.heavenstea.com/',
-    description: 'Tea is about making every moment special. We offer Public Tea Classes, Rare Tastings, Transformational Tea Journeys, Private Healing Sessions, Meditation and Tea in Nature outings. We are devoted to giving you an experience of Chinese tea in relation to self cultivation, meditation, healing and in support of your creative expression in the world.',
-    latitude: 45.476545,
-    longitude: -122.537125,
-    street: '122nd Ave and SE Foster',
+    name: 'Project Lemonade',
+    shopUrl: 'http://www.projectlemonadepdx.org',
+    description: 'Project Lemonade provides foster youth with a back-to-school shopping experience and offers support to inspire self-esteem and promote success. Store is open annually in August, pre-registration required in July.',
+    street: '1006 Lloyd Center',
     city: 'Portland',
     state: 'OR',
-    zip: '97215',
+    zip: '97232',
     country: 'USA',
-    category: 'Family'
+    category: 'Resource, Youth, Family'
   },
   {
-    name: 'Carrie and Samantha Johnson',
-    shopUrl: 'http://www.taooftea.com/',
-    description: 'Our focus is to offer teas from the Camellia Sinensis tea plants. Similar to grapes, the tea plants have many regions, varietals, processing methods, and seasons. More importantly, good tea sells out very quickly and is seldom made in large quantities. We as merchants are particular in working with growers to secure exceptional-tasting leaf each season. We pledge to share with you the artistry of the leaves, their origin, processing method, flavor vocabulary, and spirit.',
-    latitude: 45.516211,
-    longitude: -122.628746,
-    street: '3430 SE Belmont St',
+    name: 'Bridge Meadows',
+    shopUrl: 'https://www.bridgemeadows.org/',
+    description: 'Bridge Meadows develops and sustains intergenerational neighborhoods for adoptive families of youth formerly in foster care that promote permanency, community and caring relationships while offering safety and meaningful purpose in the daily lives of older adults. Youth coming from foster care find loving adoptive parents, honorary grandparents and valued mentors.',
+    street: '8502 N. Wayland Ave.',
     city: 'Portland',
     state: 'OR',
-    zip: '97214',
+    zip: '97203',
     country: 'USA',
-    category: 'Family'
+    category: 'Family, Housing'
   },
   {
-    name: 'Keeley Hammond and David Rappoccio',
-    shopUrl: 'https://songtea.com/',
-    description: 'Each year, we assemble a collection of traditional, rare and experimental tea from China and Taiwan. We look for skillfully crafted leaves from clean growing regions – teas with structure, texture, and complexity. More importantly, every tea in the collection needs to be delicious.',
-    street: '2120 Sutter Street',
-    city: 'San Francisco',
-    state: 'CA',
-    zip: '94115',
+    name: 'Bridge Meadows',
+    shopUrl: 'http://www.bridgemeadowsbeavertonapts.com/',
+    description: 'Bridge Meadows develops and sustains intergenerational neighborhoods for adoptive families of youth formerly in foster care that promote permanency, community and caring relationships while offering safety and meaningful purpose in the daily lives of older adults. Youth coming from foster care find loving adoptive parents, honorary grandparents and valued mentors.',
+    street: '5995 SW Menlo Drive',
+    city: 'Beaverton',
+    state: 'OR',
+    zip: '97005',
     country: 'USA',
-    category: 'Family'
+    category: 'Family, Housing'
   },
   {
-    name: 'Aung Si Sung',
-    shopUrl: 'http://www.songfangtea.com/',
-    description: 'Song Fang Maison de Thé specializes in the finest quality Chinese and French teas for sale and to savor on the premises. They offer a selection of 70 premium teas from China and France.',
-    street: '227 Yongjia Rd',
-    city: 'Shanghai',
-    state: 'Xuhui Qu',
-    zip: '200020',
-    country: 'China',
-    category: 'Mentor'
-  },
-  {
-    name: 'Balaji Krishnamurthy',
-    shopUrl: '',
-    description: 'A cozy franchise shop based in the Yunnan province, specializing in oolongs.',
-    street: 'Yunfang E Rd, Xishan Qu',
-    city: 'Yunnan',
-    state: 'Kunming',
-    zip: '650034',
-    country: 'China',
-    category: 'Mentor'
-  },
-  {
-    name: 'Susan Albright',
-    shopUrl: 'http://www.lockcha.com/',
-    description: 'Tranquil outfit in the middle of a park offering vegetarian dim sum dishes & premium teas.',
-    street: 'Man Shing Industrial Building Suite 1601, 307-311',
-    city: 'Hong Kong',
-    state: '',
-    zip: '',
-    country: 'China',
-    category: 'Family'
-  },
-  {
-    name: 'Wisteria Tea House',
-    shopUrl: 'http://www.wistariateahouse.com/',
-    description: 'Wistaria House is a well-known and historically important teahouse located in the Daan District, of Taipei, Taiwan. The establishment is situated in a Japanese-style wooden house built in the 1920s on Xinsheng South Road. The teahouse is named after the three wisteria vines planted in the front courtyard forming a shaded area leading to the entrance of the building.',
-    street: 'No. 1, Lane 16, Section 3, Xinsheng South Road, Da’an District',
-    city: 'Taipei',
-    state: '',
-    zip: '106',
-    country: 'Taiwan',
-    category: 'tea house'
-  },
-  {
-    name: 'Lin Ceramic\'s Studio',
-    shopUrl: 'http://www.aurlia.com.tw/theme.php',
-    description: 'Starting from make a small pot, it has now become an innovative brand name in tea wares in Great China. The classic kettle set has become an essential ware for teahouses as well as an important element in tea culture. In addition, Lin’s Ceramics Studio was also a sponsor of the Tea Serving Presentation in Taiwan Pavilion Expo 2010 Shanghai China.',
-    street: 'No. 8, Lane 6, Yongkang Street, Da’an District',
-    city: 'Taipei',
-    state: '',
-    zip: '106',
-    country: 'Taiwan',
-    category: 'tea shop'
-  },
-  {
-    name: 'Far West Tea Traders',
-    shopUrl: 'http://farwesttea.com/',
-    description: 'We only procure teas that stand out with a distinctive and memorable character and supply them directly to you at the lowest possible price.  We are a company dedicated to enriching people’s lives through the love of tea and the alchemy of tea time.',
-    street: 'No. 8, Lane 6, Yongkang Street, Da’an District',
-    city: 'Santa Barbera',
-    state: 'CA',
-    zip: '',
+    name: 'Q Center',
+    shopUrl: 'http://www.pdxqcenter.org/resources/',
+    description: 'Q Center provides a safe space to support and celebrate LGBTQ diversity, equity, visibility and community building. Q Center is a 501c3 non-profit organization which offers multi-generational programs and services in four core areas: Arts & Culture; Education & Training; Health & Wellness and Advocacy. There are dozens of events and groups that meet here, some focused on specific topics (like coming out or gender identity) and others aimed at fun social interactions (like sing-along piano cabarets or craft nights). Entry to most of these is sliding-scale and run by dedicated volunteers, keeping Q Center truly community-driven and accessible. In addition to our programmatic offerings, our 5,000 sq/ft facility is also a safe and friendly place to spend an afternoon. With our monthly art exhibits, extensive library collection, Resource Wall, and free WiFi access, there’s always something to see and learn here at Q Center.',
+    street: '4115 N. Mississippi Ave.',
+    city: 'Portland',
+    state: 'OR',
+    zip: '97217',
     country: 'USA',
-    category: 'tea production'
+    category: 'Family, Youth, LGBTQ'
   },
   {
-    name: 'Wan Ling Tea House',
-    shopUrl: 'http://www.wanlingteahouse.com/section.php/116/1/shanghai-tea-house',
-    description: 'Wan Ling Tea House is the coming together of tea lovers from opposite sides of the world. Our aim is to bring carefully selected loose leaf teas and a wealth of collected information together. Giving people the chance to better understand and explore the fascinating world of tea. And our actual ‘brick and mortar’ Shanghai tea house & tea shop.',
-    street: 'Xizang Middle Rd, 268',
-    city: 'Shanghai',
-    state: 'Huangpu Qu',
-    zip: '200000',
-    country: 'China',
-    category: 'tea house'
+    name: 'Pivot Job Corps',
+    shopUrl: 'https://pivot.jobcorps.gov/',
+    description: 'Partners in Vocational Opportunity Training (PIVOT) Job Corps Center is a great place for young people to complete an education, learn a marketable trade, make lifetime friends, and graduate with a good job in hand. Through our Career Development Services System (CDSS), we provide our students with the skills you need to succeed in today’s workforce—at no cost to you or your family. If you are looking for a better quality of life and are willing to dedicate yourself to our life changing program, Partners in Vocational Opportunity Training (PIVOT) Job Corps Center is the place for you!',
+    street: '2701 NW Vaughn St #151',
+    city: 'Portland',
+    state: 'Oregon',
+    zip: '97210',
+    country: 'USA',
+    category: 'Job services, Education, Youth'
   },
-  {
-    name: 'Springs and Autumns',
-    shopUrl: 'http://www.springsandautumns.com/',
-    description: 'Based in Taipei\'s cultural district, Springs and Autumns is an online store sourcing teaware and objects of daily use from Taiwan and East Asia.',
-    street: 'Online Retailer',
-    city: 'Taipei',
-    state: '',
-    zip: 'Taiwan',
-    country: '106',
-    category: 'tea production'
-  },
-  {
-    name: 'Red Blossom Tea Company',
-    shopUrl: 'https://www.redblossomtea.com/',
-    description: 'Red Blossom Tea Company is first and foremost a family owned tea shop in San Francisco\'s Chinatown, but we are also much more. We are direct importers, traveling thousands of miles in search of rare teas and artisan tea ware. We are educators, striving to bring the richness of traditional tea culture to our customers. And most importantly, we are tea lovers. We love to drink tea, talk tea, think tea and even obsess over tea.',
-    street: '831 Grant Ave, CA 94108',
-    city: 'San Francisco',
-    state: 'CA',
-    zip: '94108',
-    country: 'united states',
-    category: 'tea shop'
-  }
+  
 ];
 
 export default data;
--- src/data/families.js
+++ src/data/families.js
@@ -1,34 +1,41 @@
 const families = [
   {
-    name: 'Song Tea & Ceramics',
-    website: 'https://songtea.com/',
-    description: 'Each year, we assemble a collection of traditional, rare and experimental tea from China and Taiwan. We look for skillfully crafted leaves from clean growing regions – teas with structure, texture, and complexity. More importantly, every tea in the collection needs to be delicious.',
-    email: 'me@me.com',
-    categories: ['oolong', 'teaware', 'puer']
+    name: 'Office of Student Access and Completion',
+    website: 'https://oregonstudentaid.gov/finaid-foster-youth.aspx',
+    description: 'If you have been involved in the foster care system and have financial need, OSAC can help you navigate the process of planning and paying for college. Find information on tuition waivers, financial aid, and more. They also offer a variety of topical podcasts.',
+    categories: ['education']
   },
   {
-    name: 'Far West Tea',
-    website: 'http://farwesttea.com/',
-    description: 'Far West Tea Trader tea is sourced on a seasonal basis from a single day’s harvest, batch to batch. I personally select loose leaf teas that stand out with a distinctive and memorable character.',
-    categories: ['oolong', 'teaware', 'puer']
+    name: 'PCC Fostering Success',
+    website: 'https://www.pcc.edu/fostering-success/',
+    description: 'Our program provides a support network and individual coaching to enable students who have experienced foster care to successfully complete career and educational goals.',
+    email: 'fosteringsuccess@pcc.edu',
+    categories: ['education']
   },
   {
-    name: 'West China Tea Company',
-    website: 'http://westchinateacompany.com/',
-    description: 'We specialize in farm-direct tea and artisan tea ware that we obtain by going directly to the source.',
-    categories: ['oolong', 'teaware', 'puer']
+    name: 'Morrison Child & Family Services Foster Family Care Network',
+    website: 'http://www.morrisonkids.org/foster-parent-recruitment/',
+    description: 'The FFCN recruits, trains and supports foster and respite care providers for Morrison’s foster care programs. Morrison’s Foster Family Care Network embraces diversity of all kinds in our foster parents. What all of our foster homes have in common is a desire to learn how to care for kids with trauma, a willingness to share their life experience and home, and a desire to be part of a healing team for a child or teen. Morrison provides its foster parents with excellent training, superior support, generous tax free-monthly stipends and the appreciation of a professional therapeutic team.',
+    categories: ['foster family support']
   },
   {
-    name: 'Daoist Meditation',
-    website: 'http://daoistmeditation.com/shop/',
-    description: 'We are a cooperative project based in Canada, China, and Taiwan. The inventory changes several times a year and we pride ourselves on having teas that are not available anywhere else. Most of our products are certified organic, wild harvested, or aged, and all of them are wonderful. We pride ourselves on direct cooperation with farmers, tea masters, and clients, and avoid wholesalers, except in the case of aged craft teas that are held on to by various collector friends of ours.',
-    categories: ['oolong', 'teaware', 'puer']
+    name: 'Boys and Girls Aid',
+    website: 'https://www.boysandgirlsaid.org/fostercare',
+    description: 'We believe in our foster parents and we want them to succeed in every way. We offer training, support and paid time off to ensure youth are placed with the best possible foster parents. As a foster parent, you will receive: -Extensive training on how to be successful as a foster parent and understand the needs of children in foster care. -24 hour support to answer questions, provide guidance and assist with emergencies so foster parents never feel alone. -A community of other like-minded foster parents where people can share ideas, advice and expertise with one another',
+    email: 'mmurphy@boysandgirlsaid.org',
+    categories: ['training, foster family support']
   },
   {
-    name: 'Tea Mountain (Czech)',
-    website: 'http://www.teamountain.cz/',
-    description: 'Tea Mountain.',
-    categories: ['oolong', 'teaware', 'puer']
+    name: 'Child Welfare Information Gateway Foster Parent Inservice Training)',
+    website: 'https://www.childwelfare.gov/topics/management/training/curricula/foster/foster/',
+    description: 'Resources address online curricula and materials offering inservice training to foster families on topics such as behavior management, sibling issues, independent living, and more. ',
+    categories: ['training']
+  },
+  {
+    name: 'FosterClub Foster Parent Online Training',
+    website: 'https://www.fosterclub.com/foster-parent-training',
+    description: 'Our online training pairs information from leading child welfare experts with authentic youth perspective. FosterClub training is trauma-informed, includes a forum where foster parents can learn from each other, provides access to exlusive FosterClub tools and is low-cost and convenient. Course is $24.95 for 1 year subscription per parent (unlimited access to courses for one year).',
+    categories: ['training']
   },
 
 ];
